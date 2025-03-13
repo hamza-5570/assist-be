156a72d474cb0dbfc6a540114071802127aa76b9
Muhammad Ali Naeem <alinaeemgohan1228@yahoo.com>
2025-03-13 17:11:42 +0500

    changes

--- utils/Emails.js
+++ utils/Emails.js
@@ -1,22 +1,22 @@
 import nodemailer from "nodemailer";
 
-// const transport = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
-//   },
-// });
-
-// Looking to send emails in production? Check out our Email API/SMTP product!
-var transport = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+const transport = nodemailer.createTransport({
+  service: "gmail",
   auth: {
-    user: "395668addd5a57",
-    pass: "e67201ffc4bf1a",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
+
+// Looking to send emails in production? Check out our Email API/SMTP product!
+// var transport = nodemailer.createTransport({
+//   host: "sandbox.smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: "395668addd5a57",
+//     pass: "e67201ffc4bf1a",
+//   },
+// });
 
 const sendMail = async (receiverEmail, subject, body) => {
   await transport.sendMail({
