679bbd6b63d3436dbc64a1c47eb601d9fddaf57b
Muhammad Ali Naeem <alinaeemgohan1228@yahoo.com>
2025-04-11 17:12:35 +0500

    changes

--- config/localUpload.js
+++ config/localUpload.js
@@ -1,26 +1,26 @@
-import multer from "multer";
-import path from "path";
-import fs from "fs";
+// import multer from "multer";
+// import path from "path";
+// import fs from "fs";
 
-const uploadPath = "uploads/";
+// const uploadPath = "uploads/";
 
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
+// if (!fs.existsSync(uploadPath)) {
+//   fs.mkdirSync(uploadPath, { recursive: true });
+// }
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadPath);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, uniqueSuffix + path.extname(file.originalname));
+//   },
+// });
 
-const uploadLocal = multer({
-  storage,
-  limits: { fileSize: 100 * 1024 * 1024 },
-});
+// const uploadLocal = multer({
+//   storage,
+//   limits: { fileSize: 100 * 1024 * 1024 },
+// });
 
-export default uploadLocal;
+// export default uploadLocal;
